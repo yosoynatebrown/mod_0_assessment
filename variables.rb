fbb67cd629319df8797ac123676fdc67163370ad
Nathan Brown <nathanbrown@Nathans-MacBook-Pro.local>
2021-07-30 17:10:41 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+animal = "lion"
+is_cuddly = false
+animal = "koala"
+num_8 = 15
+num_3 = 7
+quotient = num_3 / num_8
+favorite_films = ["The Matrix", "Before Sunrise", "Le Samourai"]
+favorite_films.shift
+p animal.length
