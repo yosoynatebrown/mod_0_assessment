b0942e047977fbe0a258dfb76512a3b37c5c3384
Nathan Brown <nathanbrown@Nathans-MacBook-Pro.local>
2021-07-30 17:21:11 -0600

    add model A hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+laptop_details = {
+  brand: "Apple",
+  model: "Macbook Pro",
+  screen_size_inches: 15.5,
+  price_in_USD: 1200,
+  unix_based: true,
+  retina_display: true,
+  accessories: ["external mouse", "external keyboard", "external monitor"],
+  default_apps: ["Safari", "Photos", "iCloud"]
+}
