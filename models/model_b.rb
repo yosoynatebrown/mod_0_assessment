f1c800273f5a136e633f0e1e04e2511a42cedb4d
Nathan Brown <nathanbrown@Nathans-MacBook-Pro.local>
2021-07-30 17:31:31 -0600

    Add model B hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+house_details = {
+  "city" => "Louisville",
+  "state" => "Kentucky",
+  "square_feet" => 3000,
+  "price in USD" => 500000.75,
+  "single family" => true,
+  "fixer_upper" => false,
+  "exterior paint colors" => ["Lavender", "Navy Blue", "Cerulean"],
+  "interior paint colors" => ["Indigo", "Peach", "White"]
+}
